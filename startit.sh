b3097d3711ec36e649517dd4007d0b995c2f2b4b
bob bae <bob.bae@gmail.com>
2020-04-23 10:17:54 -0700

    st

---
+++ startit.sh
@@ -0,0 +1,8 @@
+jupyter notebook
+# set FLASK_ENV=development
+# #set FLASK_APP=app.py 
+# flask run
+
+# python3 -m venv C:/Users/bobba/venv-lacovid
+# pip install ...
+# pip freeze > requirements.txt
