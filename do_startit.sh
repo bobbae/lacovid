52c12f64f2621782ebe5d54d3daf7c29fa8cfb32
bob bae <bob.bae@gmail.com>
2020-05-27 15:29:24 -0700

    mac

--- do_startit.sh
+++ do_startit.sh
@@ -1,4 +1,6 @@
-jupyter notebook
+export DATABASE_URL=`heroku config:get DATABASE_URL -a aaa12w3 -j`
+#jupyter notebook
+jupyter lab
 # set FLASK_ENV=development
 # #set FLASK_APP=app.py 
 # flask run
